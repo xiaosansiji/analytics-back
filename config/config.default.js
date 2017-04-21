5242ae933f730f4491b93c74fc448417d46cb7f9
sunzhe.it@haier.com <12345678>
2017-04-21 15:37:57 +0800

    project init

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1492759453770_3426';
+
+  return config;
+};
